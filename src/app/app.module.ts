1eb23749a09572ba54808b8ea9b4e871d196b9d5
Artur Zurek <artur.zurek@ssginsight.com>
2019-06-23 19:16:00 +0200

    Botstrap added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AttributesFormComponent } from './attributes-form/attributes-form.component';
+import bootstrap from "bootstrap";
 
 @NgModule({
   declarations: [
